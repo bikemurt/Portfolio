e60ce6f66c79991b144c01fa1184978615dd0cbb
Michael Burt <23486102+bikemurt@users.noreply.github.com>
2023-11-29 13:47:19 -0500

    updates

--- src/scroller.js
+++ src/scroller.js
@@ -14,8 +14,38 @@ $(document).ready(function () {
     }
 });
 
+function loadMenu(menuPath, menuID)
+{
+    $.ajax({
+        url: menuPath, // Replace with the path to your text file
+        dataType: 'text',
+        success: function (data) {
+            // Update the content of the div with the text from the file
+            $('#' + menuID).html(data);
+    
+            $("#" + menuID + " a").each(function()
+            {
+                if (this.href == window.location.href)
+                {
+                    this.parentNode.classList.add('current-page');
+                }
+            });
+        },
+        error: function (error) {
+            console.error('Error loading text file:', error);
+        }
+    });
+}
+
+loadMenu('/components/menu.txt', 'menu')
+
+if ($("#menu3dart").length)
+{   
+    loadMenu('/components/menu3dart.txt', 'menu3dart')
+}
+
 $.ajax({
-    url: '../components/footer.txt', // Replace with the path to your text file
+    url: '/components/footer.txt', // Replace with the path to your text file
     dataType: 'text',
     success: function (data) {
         // Update the content of the div with the text from the file

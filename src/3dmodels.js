700a3d663ebf60e0b25b2aac523f52561d0464f2
Michael Burt <23486102+bikemurt@users.noreply.github.com>
2023-12-12 10:07:55 -0500

    updates

--- src/3dmodels.js
+++ src/3dmodels.js
@@ -119,7 +119,7 @@ function viewport3d(container, model, camx = 1.3, camy = 0.1, camz = -0.7)
     
 }
 
-viewport3d('model-container1', './models/Case.gltf', 2, 0.6, 1.9);
-viewport3d('model-container2', './models/Generator.gltf', -1.6, 0.2, -1.5);
-viewport3d('model-container3', './models/Scope.gltf', 1.9, 0.1, 0.9);
-viewport3d('model-container4', './models/Crate.gltf', 1.4, 0.9, -0.5);
+viewport3d('model-container1', '../models/Case.gltf', 2, 0.6, 1.9);
+viewport3d('model-container2', '../models/Generator.gltf', -1.6, 0.2, -1.5);
+viewport3d('model-container3', '../models/Scope.gltf', 1.9, 0.1, 0.9);
+viewport3d('model-container4', '../models/Crate.gltf', 1.4, 0.9, -0.5);

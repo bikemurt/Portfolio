c22be1ea3f5df3f569cc76e50e6f20655e0dc774
Michael Burt <23486102+bikemurt@users.noreply.github.com>
2024-05-26 12:18:21 -0400

    add comm tower

--- src/3d/model7.js
+++ src/3d/model7.js
@@ -58,37 +58,37 @@ function viewport3d(container, model, camx = 1.3, camy = 0.1, camz = -0.7)
         scene.add(model);
     
         // Create a directional light
-        const directionalLight = new THREE.DirectionalLight(0xffffff, 3);
+        const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
         directionalLight.position.set(4, 5, 5); // Set the position of the light
         scene.add(directionalLight);
     
-        const directionalLight2 = new THREE.DirectionalLight(0xffffff, 2);
+        const directionalLight2 = new THREE.DirectionalLight(0xffffff, 1);
         directionalLight2.position.set(-5, 3, 5); // Set the position of the light
         scene.add(directionalLight2);
     
-        const directionalLight3 = new THREE.DirectionalLight(0xffffff, 2);
+        const directionalLight3 = new THREE.DirectionalLight(0xffffff, 1);
         directionalLight3.position.set(6, -5, 5); // Set the position of the light
         scene.add(directionalLight3);
     
-        const directionalLight4 = new THREE.DirectionalLight(0xffffff, 2);
+        const directionalLight4 = new THREE.DirectionalLight(0xffffff, 1);
         directionalLight4.position.set(5, 3, -5); // Set the position of the light
         scene.add(directionalLight4);
     
     
-        const width = 1;
-        const height = 1;
+        const width = 10;
+        const height = 10;
         const intensity = 3;
         const rectLight = new THREE.RectAreaLight( 0xffffff, intensity,  width, height );
         rectLight.position.set( -2, 2, 3 );
         rectLight.lookAt( 0, 0, 0 );
-        //scene.add( rectLight )
+        scene.add( rectLight )
         
         const rectLightHelper = new RectAreaLightHelper( rectLight );
         //rectLight.add( rectLightHelper );
     
         
-        const rectLight2 = new THREE.RectAreaLight( 0xffffff, intensity * 1000.0,  width, height );
-        rectLight2.position.set(1,-1 ,1);
+        const rectLight2 = new THREE.RectAreaLight( 0xffffff, intensity,  width, height );
+        rectLight2.position.set( -3, -5, 0 );
         rectLight2.lookAt( 0, 0, 0 );
         //scene.add( rectLight2 )
         
@@ -106,7 +106,7 @@ function viewport3d(container, model, camx = 1.3, camy = 0.1, camz = -0.7)
             if (getcam)
             {
                 count++;
-                if (count >= 60) // every 10s display the cam position
+                if (count >= 600) // every 10s display the cam position
                 {
                     //console.log(container + " -> " + controls.object.position.x + ","+controls.object.position.y+","+controls.object.position.z);
                     console.log(container + " -> " + camera.position.x + ","+camera.position.y+","+camera.position.z);
@@ -121,4 +121,4 @@ function viewport3d(container, model, camx = 1.3, camy = 0.1, camz = -0.7)
 }
 
 
-viewport3d('model-container1', '/models/CommsTower.gltf', 1.04, -0.25, 1.01);
+viewport3d('model-container1', '/models/Zombie.gltf', -0.043, -0.155, 0.765);
